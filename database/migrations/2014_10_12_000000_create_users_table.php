f05f6c9cdb993445fda9957932099885f83ce34a
Tamara Hall <tamarahall@Tamaras-MBP.lan>
2018-02-25 18:23:59 -0500

    Adding administrator column to user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,8 +18,11 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->boolean('administrator')->default(0);
             $table->rememberToken();
             $table->timestamps();
+            
+
         });
     }
 
