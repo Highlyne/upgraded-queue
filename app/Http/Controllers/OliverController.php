b91782b6794896aab27115d3df43ffa617fd37f1
Tamara Hall <tamarahall@Tamaras-MBP.lan>
2018-02-25 10:10:28 -0500

    barber pic is showing up on res_form

--- app/Http/Controllers/OliverController.php
+++ app/Http/Controllers/OliverController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Jobs;
 
 class OliverController extends Controller
 {
@@ -14,7 +15,10 @@ class OliverController extends Controller
     public function index()
     {
         //
-        return view('book.index');
+        $barberInfo = array("Oliver", "<a id='w13R1AF9RptoZwLVt8YE0g' class='gie-single' href='http://www.gettyimages.com/detail/82796066' target='_blank' style='color:#a7a7a7;text-decoration:none;font-weight:normal !important;border:none;display:inline-block;'>Embed from Getty Images</a><script>window.gie=window.gie||function(c){(gie.q=gie.q||[]).push(c)};gie(function(){gie.widgets.load({id:'w13R1AF9RptoZwLVt8YE0g',sig:'spIwmlN1WwohFB0PFmR24OxB8aOpBV0ckvQYZaBjc-A=',w:'431px',h:'398px',items:'82796066',caption: true ,tld:'com',is360: false })});</script><script src='//embed-cdn.gettyimages.com/widgets.js' charset='utf-8' async></script>");
+        // This function will return the booking option
+        $appts = Jobs::where('barber', 'Oliver')->get();
+        return view('book.res_form', compact('appts', 'barberInfo'));
     }
 
     /**
@@ -25,6 +29,14 @@ class OliverController extends Controller
     public function create()
     {
         //
+        $jobs = App\Jobs::forceCreate([
+            'id' => '1',
+            'book_date' => '2018-03-01',
+            'time' => '11:00:00',
+            'name' => 'Peter Parker',
+            'barber' => 'Oliver',
+            'confirmed' => '0'
+            ]);
     }
 
     /**
--- resources/views/book/res_form.blade.php
+++ resources/views/book/res_form.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.app')
 @section('content')
-<h1>Ready to book</h1>
+<h3>Book an appointment with</h3>
+<h1>{{ $barberInfo[0] }} </h1>
     <hr>
-<p>Choose a barber from below to schedule an appointment with</p>
 <br>
 
 <div class="container-fluid">
     <div class="row">
         <div class="col-sm-4">
-                <img src='{{$barberPic}}'> 
-                <img src="https://www.istockphoto.com/photo/little-boy-getting-his-head-shaved-by-barber-gm464406044-58482742">
+                <img src={{!! $barberInfo[1] !!}}> 
+                
         </div>
         <div class="col-sm-8">
             <div class="row">
@@ -40,73 +40,5 @@
     </div>
 </div>
 <hr>
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-sm-4">
-            <div id="oliver">
-          <a id='XYHHj252R8Z9_uLAWqAnGw' class='gie-single' href='http://www.gettyimages.com/detail/82796066' target='_blank' style='color:#a7a7a7;text-decoration:none;font-weight:normal !important;border:none;display:inline-block;'>Embed from Getty Images</a><script>window.gie=window.gie||function(c){(gie.q=gie.q||[]).push(c)};gie(function(){gie.widgets.load({id:'XYHHj252R8Z9_uLAWqAnGw',sig:'ykobd1GYwfmtHNA4DT0vq_0nmJzxtBCD6Vxgjz4q3So=',w:'431px',h:'398px',items:'82796066',caption: true ,tld:'com',is360: false })});</script><script src='//embed-cdn.gettyimages.com/widgets.js' charset='utf-8' async></script>
-          <a onclick="showBarberAvail()"class="btn btn-info" role="button">Oliver Short</a>
-          </div>
-        <div class="col-sm-4">
-            <div id="theresa">
-        <a id='7wfQaU-1S6VpsgQ4mg7sNg' class='gie-single' href='http://www.gettyimages.com/detail/724228177' target='_blank' style='color:#a7a7a7;text-decoration:none;font-weight:normal !important;border:none;display:inline-block;'>Embed from Getty Images</a><script>window.gie=window.gie||function(c){(gie.q=gie.q||[]).push(c)};gie(function(){gie.widgets.load({id:'7wfQaU-1S6VpsgQ4mg7sNg',sig:'909FKwKhYAoKo70fZkgEsr3NWekRQnyycpky32A6LkU=',w:'431px',h:'398px',items:'724228177',caption: false ,tld:'com',is360: false })});</script><script src='//embed-cdn.gettyimages.com/widgets.js' charset='utf-8' async></script>
-        <a href="/book/theresa" class="btn btn-info" role="button">Theresa Smith</a>  
-            </div>  
-    </div>
-        <div class="col-sm-4">
-            <div id="greg">
-        <a id='nt7IUQnQSL5YPa6-qQhcQA' class='gie-single' href='http://www.gettyimages.com/detail/573106855' target='_blank' style='color:#a7a7a7;text-decoration:none;font-weight:normal !important;border:none;display:inline-block;'>Embed from Getty Images</a><script>window.gie=window.gie||function(c){(gie.q=gie.q||[]).push(c)};gie(function(){gie.widgets.load({id:'nt7IUQnQSL5YPa6-qQhcQA',sig:'QHX6gni8hgpghZXOEP6uvXyRpfIEe3F1EEJor0QYcck=',w:'431px',h:'398px',items:'573106855',caption: true ,tld:'com',is360: false })});</script><script src='//embed-cdn.gettyimages.com/widgets.js' charset='utf-8' async></script>
-        <a href="/book/greg" class="btn btn-info" role="button">Greg Brown</a>  
-            </div>  
-    </div>
-    </div>
-    <div id="myDIV">
-            This is my DIV element.
-        </div>
-            
-    <div class="row">
-        <div class="col-sm-3"> </div>
-      <div class="col-sm-3"> 
-          <form action="/action_page.php">
-            Select a day: <input type="date" name="res_day">
-
-      </div>
-      <div class="col-sm-3">
-            Select a time:
-            <input type="time" name="usr_time">
-      </div>
-      <div class="col-sm-3"> </div>
-    </div>
-
-
-    <hr>
-        
-    
-        <div class="row">
-                <div class="col-sm-12">
-        <table>
-                    <thead>
-            <tr>
-                <th>Date</th>
-                <th>Time</th>
-                <th>Name</th>
-            </tr>
-        </thead>
-        <tbody>
-        @foreach ($appts as $appt)
-        <tr>
-            <td>{{ $appt->book_date }}</td>
-            <td>{{ $appt->time }}</td>
-            <td>{{ $appt->name }}</td>
-        </tr>
-        @endforeach
-    </tbody>
-        </table>
-        </div>
-    </div>
-
-</div>
-
-
 
 @stop
