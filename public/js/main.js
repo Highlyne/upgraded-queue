16d80ef22e28e0200f7f5baa457e57cf0e1f3a82
Tamara Hall <tamarahall@Tamaras-MBP.lan>
2018-03-06 10:51:46 -0500

    Setting up for deployment

--- public/js/main.js
+++ public/js/main.js
@@ -4,10 +4,7 @@ console.log("The manin js file is linked to the app");
 
 $(document).ready(function(){
 
-    function showBarberAvail(){  
-        // $("#theresa", "#greg").hide();
-        $("myDIV").show();
-        $("#greg").hide();
-    }
+    
+    
 })
 
--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -2,20 +2,64 @@
 
 @section('content')
 
-<div class="box">
-    <form id="myform" onsubmit="emailjs.sendForm('default_service', 'hot_cutz_contact', this); return false;" method="post">
-        <label>reply_to</label>
-        <input type="text" name="reply_to" />
-        <label>from_name</label>
-        <input type="text" name="from_name" />
-        <label>message_html</label>
-        <input type="text" name="message_html" />
-        <br><br>
-        <button>
-        Send
-        </button>
-    </form>
-</div>       
+<div class="fb-page" data-href="https://www.facebook.com/facebook" data-tabs="timeline" data-width="500" data-height="400" 
+data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="false">
+<blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore">
+    <a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div>
+{{--  Send and email content  --}}
+<span class="emial">
+    <div class="Columns">  
+        <div class="column">
+            <div class="box">
+                <h2> Send an Email </h2>
+                <form id="myform" onsubmit="emailjs.sendForm('default_service', 'hot_cutz_contact', this); return false;" method="post">
+                    <div class="field-label is-normal is-left">
+                        <label class="label">Email</label>
+                    </div>
+                    <div class="field-body">
+                        <div class="field">
+                            <p class="control">
+                            <input class="input" name="client_email" type="email" placeholder="Your email">
+                            </p>
+                        </div>
+                    </div>
+                    <div class="field-label is-normal">
+                        <label class="label">Name</label>
+                        </div>
+                    <div class="field-body">
+                        <div class="field">
+                            <p class="control">
+                            <input class="input" name="client_name" type="text" placeholder="Your name">
+                            </p>
+                        </div>
+                    </div>
+                    <div class="field-label is-normal is left">
+                        <label class="label">Message</label>
+                        </div>
+                    <div class="field-body">
+                        <div class="field">
+                            <p class="control">
+                            <textarea class="textarea is-large" type="text" name="message_html" placeholder="As a valued client, I need assistance with:"></textarea>
+                            </p>
+                        </div>
+                    </div>
+                <br><br>
+                    <button>
+                    Send
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>       
+</span>
+{{--  Modal data  --}}
+<div class="modal">
+    <div class="modal-background"></div>
+        <div class="modal-content">
+            Thank you! Your email has been sent
+        </div>
+    <button class="modal-close is-large" aria-label="close"></button>
+</div>
         <script type="text/javascript" src="https://cdn.emailjs.com/dist/email.min.js"></script>
         <script type="text/javascript">
            (function(){
@@ -23,5 +67,12 @@
            })();
         </script>
 
+        <script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12';
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
 
 @endsection
